9bdf5c1482137eb702e0196418aa8a31f7dfef21
alvinle2901 <bach938@gmail.com>
2023-11-12 05:42:04 +0200

    fix ui

---
+++ client/src/constants/data.ts
@@ -0,0 +1,22 @@
+export const items = [
+  {
+    img: 'https://images.unsplash.com/photo-1583416750470-965b2707b355?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    name: 'Going to Sauna',
+    id: 1
+  },
+  {
+    img: 'https://plus.unsplash.com/premium_photo-1661775884128-d971ffd2d300?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    name: 'Go to sleep early',
+    id: 2
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1599577456698-e1e9ae4f4e5b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    name: 'Back Exercises',
+    id: 3
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1600679472233-eabc13b79f07?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    name: 'Drink enough water',
+    id: 4
+  }
+]
